06822730b69bb81d295174f16e56c8ea39c32819
cqc <1016903103@qq.com>
2016-11-21 16:18:06 +0800

    update auth

--- pppoe.py
+++ pppoe.py
@@ -2,10 +2,41 @@ import re
 from subprocess import check_output, CalledProcessError
 
 from flask import Flask
+from functools import wraps
+from flask import request, Response
 
 cmd_start = 'pppoe-start'
 cmd_stop = 'pppoe-stop'
 cmd_status = 'pppoe-status'
+need_auth = True
+auth_user = 'admin'
+auth_pass = '123456'
+
+
+def check_auth(username, password):
+    if need_auth:
+        return username == auth_user and password == auth_pass
+    else:
+        return True
+
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+        'Could not verify your access level for that URL.\n'
+        'You have to login with proper credentials', 401,
+        {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+
+    return decorated
 
 
 class Pppoe():
@@ -36,12 +67,12 @@ pppoe = Pppoe()
 
 
 @app.route('/repppoe')
+@requires_auth
 def repppoe():
     result = pppoe.repppoe()
-    print(result)
     ip = pppoe.get_ip(result)
     return ip
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
